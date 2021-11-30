e2a6b6571cbaecc57eb4f54c2a110a1aaeeb4fc6
chanchann <www.769440972@qq.com>
2021-11-30 21:43:07 +0800

    test(tests): add StrUtil test

--- tests/StringPiece_feature.cc
+++ tests/StringPiece_feature.cc
@@ -2,31 +2,17 @@
 
 using namespace wfrest;
 
-void test01()
+int main()
 {
-    fprintf(stderr, "test 01\n");
-    StringPiece str("host=admin&password=123&time=10000101");
-
-    std::vector<std::string> str_list = StrUtil::split_piece<std::string>(str, '&');
-    for(auto &s : str_list)
-    {
-        fprintf(stderr, "%s\n", s.c_str());
-    }
-}
-
-void test02()
-{
-    fprintf(stderr, "test 02\n");
     StringPiece str(R"(Content-Disposition: form-data; name="avatar"; filename="user.jpg")");
     // StringPiece feature : only a watcher, still find the last '\0'
     std::vector<StringPiece> list = StrUtil::split_piece<StringPiece>(str, ';');
-
-    fprintf(stderr, "string piece feature : a watcher\n");
     for(auto &l : list)
     {
         fprintf(stderr, "dispo : %s\n", l.data());
     }
     fprintf(stderr, "\nas_string()\n");
+
     // please watch this to compare with the above
     for(auto &l : list)
     {
@@ -41,10 +27,5 @@ void test02()
             fprintf(stderr, "k : %s\n", k.as_string().c_str());
         }
     }
-}
-
-int main()
-{
-    test01();
-    test02();
+    return 0;
 }
