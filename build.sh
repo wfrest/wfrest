6aae36ecc5aad613a1a7bacb3e7b9adda8632941
chanchann <www.769440972@qq.com>
2021-12-30 21:28:03 +0800

    fix: fix github action && add build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+
+SOURCE_DIR=`pwd`
+BUILD_DIR=${BUILD_DIR:-./build}
+
+mkdir -p $BUILD_DIR \
+    && cd $BUILD_DIR \
+    && cmake \
+            -DWFREST_BUILD_EXAMPLES=ON \
+            -DWFREST_BUILD_BENCHMARK=ON \
+            -DWFREST_BUILD_TEST=ON \
+            $SOURCE_DIR \
+    && make $*
+
