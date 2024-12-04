54047aeb44849a746d30d36359608610510d1c0a
chanchann <www.769440972@qq.com>
2024-12-04 10:14:59 +0000

    feat : reverse aop after sequence

--- src/core/AopUtil.h
+++ src/core/AopUtil.h
@@ -21,6 +21,20 @@ for_each(std::tuple<Tp...> &tp, FuncT func)
     for_each<I + 1, FuncT, Tp...>(tp, func);
 }
 
+// 添加反向遍历的 for_each
+template<std::size_t I = 0, typename FuncT, typename... Tp>
+inline typename std::enable_if<I == sizeof...(Tp), void>::type
+for_each_reverse(std::tuple<Tp...> &, FuncT)
+{}
+
+template<std::size_t I = 0, typename FuncT, typename... Tp>
+inline typename std::enable_if<I < sizeof...(Tp), void>::type
+for_each_reverse(std::tuple<Tp...> &tp, FuncT func)
+{
+    func(std::get<sizeof...(Tp) - 1 - I>(tp));
+    for_each_reverse<I + 1, FuncT, Tp...>(tp, func);
+}
+
 class HttpReq;
 class HttpResp;
 
@@ -43,7 +57,7 @@ template<typename Tuple>
 inline bool aop_after(const HttpReq *req, HttpResp *resp, Tuple &tp)
 {
     bool ret = true;
-    for_each(
+    for_each_reverse(
             tp,
             [&ret, req, resp](Aspect &item)
             {
